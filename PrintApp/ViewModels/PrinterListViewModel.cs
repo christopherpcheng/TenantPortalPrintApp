28951ce48f8f42f8e7bdd97098b5c06c36a5bf58
kit.cheng <64066849+christopherpcheng@users.noreply.github.com>
2020-06-22 06:11:59 +0800

    Nearing Button Combo

--- PrintApp/ViewModels/PrinterListViewModel.cs
+++ PrintApp/ViewModels/PrinterListViewModel.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using PrintApp.Models;
+using PrintApp.Singleton;
+using ReactiveUI;
 
 namespace PrintApp.ViewModels
 {
     public class PrinterListViewModel : ViewModelBase
     {
+        string selectedPrinterName;
+        public string SelectedPrinterName 
+        { 
+            get => selectedPrinterName;
+            set => this.RaiseAndSetIfChanged(ref selectedPrinterName, value);
+        }
+
+
         public ObservableCollection<PrinterItem> Printers { get; }
         public List<string> PrintersL { get; set; }
+
+        public ReactiveCommand<Unit, PrinterItem> PrintCommand { get; }
+        public ReactiveCommand<Unit, Unit> PrintCommand2 { get; }
 
         public PrinterListViewModel(IEnumerable<PrinterItem> printers)
         {
@@ -21,8 +36,40 @@ namespace PrintApp.ViewModels
             {
                 PrintersL.Add(each.PrinterName);
             }
+
+            PrintCommand = ReactiveCommand.Create(
+                () => new PrinterItem { PrinterName = SelectedPrinterName }
+                );
+
+            PrintCommand2 = ReactiveCommand.Create(() => { });
+
+            Observable.Merge(
+                PrintCommand,
+                PrintCommand2.Select(_ => (PrinterItem)null))
+                .Take(1)
+                .Subscribe(model =>
+                {
+                    if (model != null)
+                    {
+                        Globals.Log("Print!");
+                        Globals.Log($"Printer Selected: {SelectedPrinterName}");
+
+                        Globals.Log($"Got:{model.PrinterName}");
+
+                    }
+                }
+
+                );
+
+
+        }
+
+        public void PrintIt()
+        {
+            Globals.Log("Print!");
+            Globals.Log($"Printer Selected: {SelectedPrinterName}");
             
-            
+
         }
     }
 
