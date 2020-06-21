1fcc8da02dc48d4642ba7c37c4911ff2f5f22568
kit.cheng <64066849+christopherpcheng@users.noreply.github.com>
2020-06-21 15:13:55 +0800

    New Project

---
+++ PrintApp/Views/PrinterListView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PrintApp.Views
+{
+    public class PrinterListView : UserControl
+    {
+        public PrinterListView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
