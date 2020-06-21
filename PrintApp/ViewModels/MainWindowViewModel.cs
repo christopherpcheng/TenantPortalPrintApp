dd9abc2fabd9ddc10e9c26b8e87b70010c71e00e
kit.cheng <64066849+christopherpcheng@users.noreply.github.com>
2020-06-22 04:53:37 +0800

    Creted VM swapper

--- PrintApp/ViewModels/MainWindowViewModel.cs
+++ PrintApp/ViewModels/MainWindowViewModel.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 using PrintApp.Services;
+using ReactiveUI;
 
 namespace PrintApp.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        ViewModelBase contentVM;
+        public ViewModelBase ContentVM
+        {
+            get => contentVM;
+            private set => this.RaiseAndSetIfChanged(ref contentVM, value);
+        }
         public PrinterListViewModel PrinterListVM { get; }
 
         public MainWindowViewModel(PrinterDatabase pdb)
         {
-            PrinterListVM = new PrinterListViewModel(pdb.GetItems());
+            ContentVM = PrinterListVM = new PrinterListViewModel(pdb.GetItems());
         }
     
     }
