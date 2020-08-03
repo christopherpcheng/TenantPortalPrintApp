2c30780ff5e6cf205347450185c2aa01b74df0fa
kit.cheng <64066849+christopherpcheng@users.noreply.github.com>
2020-08-03 19:25:19 +0800

    Update buildmacConditional.bat

--- buildmacConditional.bat
+++ buildmacConditional.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build -r osx-x64 -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:Configuration=Release-OSX .\TenantPortalPrintApp.sln 
+dotnet build -r osx-x64 -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:Configuration=Release-OSX  .\TenantPortalPrintApp.sln 
